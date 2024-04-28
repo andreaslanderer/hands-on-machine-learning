23c29323bf752ab6c81c96f240bcb5511b96c7b8
Andreas Landerer <andreaslanderer@gmail.com>
2024-04-28 17:36:23 +0200

    Chapter 02: Machine Learning project end-to-end implementation

---
+++ chapter_02_end-to-end-mp-project/load_housing_data.py
@@ -0,0 +1,14 @@
+import tarfile
+import urllib.request
+from pathlib import Path
+
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return Path("datasets/housing/housing.csv")
